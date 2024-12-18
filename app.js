1031a1188f62d92d524b7897413119e870eb1f5c
ptlong2402 <120319294+ptlong2402f35@users.noreply.github.com>
2024-12-18 18:22:30 +0700

    done

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/api", api);
+// app.use("/vnpay", express.static(path.join(__dirname, "/vnpay/")))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
